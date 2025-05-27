7c2e979e64e7624e5412e887df05a43064d9de39
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-27 22:53:54 +0100

    Created HelperFunctions.java and swap method

---
+++ javaSorts/HelperFunctions.java
@@ -0,0 +1,9 @@
+package javaSorts;
+
+public class HelperFunctions {
+    public void swap(int[] array, int index_a, int index_b) {
+        int swapper = array[index_a];
+        array[index_a] = array[index_b];
+        array[index_b] = swapper;
+    }
+}
