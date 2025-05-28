f4fa17782001033b1d0d8656448c693a34410819
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-28 12:30:23 +0100

    Created heapsort.py and made funcions left, right, max_heapify

---
+++ python/heapsort.py
@@ -0,0 +1,24 @@
+from helper_functions import swap
+
+
+def left(index):
+    return (2 * index) + 1
+
+def right(index):
+    return (2 * index) + 2
+
+def max_heapify(array, index, size):
+    left_index = left(index)
+    right_index = right(index)
+
+    if left_index < size and array[left_index] > array[index]:
+        largest = left_index
+    else:
+        largest = index
+    
+    if right_index < size and array[right_index] > array[largest]:
+        largest = right_index
+    
+    if largest != index:
+        swap(index, largest)
+        max_heapify(array, largest, size)
