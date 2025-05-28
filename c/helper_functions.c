df1c295606bcac6c7dcfaf89aefb58c958c54923
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-28 17:02:43 +0100

    Created swap() method for helper_functions.c, also made main.c

---
+++ c/helper_functions.c
@@ -0,0 +1,5 @@
+static void swap(int array[], int index_a, int index_b) {
+    int swapper = array[index_a];
+    array[index_a] = array[index_b];
+    array[index_b] = swapper;
+}
---
+++ c/main.c
@@ -0,0 +1,11 @@
+
+int main() {
+    int arr[] = {5,8,3,1,9,2,5};
+
+    swap(arr, 0, 1);
+
+    printf("%d", arr[0]);
+    printf("%d", arr[1]);
+
+    return 0;
+}
