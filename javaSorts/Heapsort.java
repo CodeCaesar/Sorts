9dc62fc8bf1846dcc1acef70a30d2715be46364b
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-28 12:41:10 +0100

    Created Heapsort.java and defined methods

---
+++ javaSorts/Heapsort.java
@@ -0,0 +1,19 @@
+package javaSorts;
+
+public class Heapsort {
+    private static int left(int[] array, int index) {
+        return (2 * index) + 1;
+    }
+
+    private static int right(int[] array, int index) {
+        return (2 * index) + 2;
+    }
+
+    private static void maxHeapify(int[] array, int index, int size) {}
+
+    private static void buildMaxHeap(int[] array, int size) {}
+
+    private static void sort(int[] array, int size) {}
+
+    public static void sort(int[] array) {}
+}
