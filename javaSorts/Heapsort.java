0c0829bf3b88bea6ad1a79c3a7c565217af62544
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-28 12:46:19 +0100

    Made maxHeapify method and deleted useless parameters on lines 4 and 8

--- javaSorts/Heapsort.java
+++ javaSorts/Heapsort.java
@@ -1,15 +1,30 @@
 package javaSorts;
 
 public class Heapsort {
-    private static int left(int[] array, int index) {
+    private static int left(int index) {
         return (2 * index) + 1;
     }
 
-    private static int right(int[] array, int index) {
+    private static int right(int index) {
         return (2 * index) + 2;
     }
 
-    private static void maxHeapify(int[] array, int index, int size) {}
+    private static void maxHeapify(int[] array, int index, int size) {
+        int leftIndex = left(index);
+        int rightIndex = right(index);
+        int largest = index;
+
+        if(leftIndex < size && array[leftIndex] > array[largest]) {
+            largest = leftIndex;
+        } else if(rightIndex < size && array[rightIndex] > array[largest]) {
+            largest = rightIndex;
+        }
+
+        if(largest != index) {
+            HelperFunctions.swap(array, index, largest);
+            maxHeapify(array, largest, size);
+        }
+    }
 
     private static void buildMaxHeap(int[] array, int size) {}
 
