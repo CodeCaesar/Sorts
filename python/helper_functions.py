1582b4b718e46b018e2c2846f9150cef60685d9c
CodeCaesar <adrian@czerwinski.info.pl>
2025-05-28 13:50:01 +0100

    Made function description of get_array() and made running time calculation more clear for is_sorted()

--- python/helper_functions.py
+++ python/helper_functions.py
@@ -13,7 +13,7 @@ def is_sorted(array):
     Function is_sorted() checks if array is sorted by comparing corrent element to the next; if current element is larger than next, 
     it returns false, if it goes throught entire array and every element is either smaller or equal to the next element that return true.
 
-    Running time: O(n) + O(n) + O(n) + O(1) = O(n)
+    Running time: O(n) + O(n) + O(n) + O(1) = O(3n + 1) = O(n)
     """
     for index in range(0, len(array) - 1):
         if array[index] > array[index + 1]:
@@ -22,6 +22,14 @@ def is_sorted(array):
     return True
 
 def get_array(array):
+        """
+        Function get_array() runs through every element of an array and adds it to string arrayElements, and returns all elements.
+        Also depending if loop is at last index it either has additional comma at the end of an element or doesn't.
+
+        Running time: O(1) + O(1) + O(n) + O(n) + O(n) + O(n) + O(n) + O(1) = O(5n + 3) = O(n)
+        However strings are immutable, thus program creates new string every loop, so the acual running time is:
+        O(1) + O(1) + O(n) + O(n) + O(n^2) + O(n) + O(n^2) + O(1) = O(2n^2 + 3n + 3) = O(n^2)
+        """
         arrayElements = ""
         followupIndex = len(array) - 1
 
