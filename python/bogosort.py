686fa1e125dddb4a438c8bdf1d9db88a0b3e2d2c
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-11 23:44:12 +0100

    Made descriptive comment for bogosort

--- python/bogosort.py
+++ python/bogosort.py
@@ -3,5 +3,21 @@ import random
 
 
 def bogosort(array:list):
+    """
+    <h1>Bogosort</h1>
+
+    <h3>Description</h3>
+    Bogosort is sorting array by randomly shuffling it until it's sorted. This is also why bogosort isn't an actual
+    algorithm since it can run forever.
+
+    <h3>Running Time</h3>
+    Best Case:\tO(3n + 1) = <b>O(n)</b> \n
+    Average Case:\t<b>O(n!)</b> \n
+    Worst Case:\t<b>Infinite</b> \n
+
+    <h3>Space Complexity & Stability</h3>
+    Space complexity is: <b>O(1)</b>
+    Bogosort is <b>not stable</b>
+    """
     while not is_sorted(array):
         random.shuffle(array)
