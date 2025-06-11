6371df4693c860c41dd734f3c3a33bd363096333
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-11 23:55:01 +0100

    Made descriptive comment for Quicksort.java

--- javaSorts/Quicksort.java
+++ javaSorts/Quicksort.java
@@ -27,6 +27,25 @@ public class Quicksort {
         }
     }
 
+    /**
+     * <h1>Quicksort</h1>
+     * 
+     * <h3>Description</h3>
+     * Quicksort is sorting array by taking last element and then comparing it each element if its smaller or greater 
+     * than last element and swaps them with pivot, later it returs pivot and sorts indexes between pivot (first, pivot-1 and pivot+1, last) 
+     * and repeats it until its sorted.
+     * 
+     * <h3>Running Time</h3>
+     * Best Case:\t<b>O(n log n)</b> \n
+     * Average Case:\t<b>O(n log n)</b> \n
+     * Worst Case:\t<b>O(n^2)</b> \n
+     * 
+     * Worst case is when array is sorted or inverted.
+     * 
+     * <h3>Space Complexity & Stability</h3>
+     * Space complexity is: <b>O(log n)</b> \n
+     * Bogosort is <b>not stable</b>
+     */
     public static void sort(int[] array) {
         sort(array, 0, array.length - 1);
     }
