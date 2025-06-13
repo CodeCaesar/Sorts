bc07d4f2522e915cecd3abb6d72351d8fa675da4
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-13 23:45:22 +0100

    Made shuffle method for bogosort

--- javaSorts/Bogosort.java
+++ javaSorts/Bogosort.java
@@ -1,8 +1,16 @@
 package javaSorts;
 
+import java.util.Random;
+
 public class Bogosort {
     
     private static void shuffle(int[] array) {
+        int randomIndex;
+
+        for(int index = 0; index < array.length; index++) {
+            randomIndex = new Random().nextInt(array.length);
+            HelperFunctions.swap(array, index, randomIndex);
+        }
     }
 
     public static void sort(int[] array) {
