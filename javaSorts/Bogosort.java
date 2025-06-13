e4d61dc364116573abf771e885d2106a3c8be616
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-13 23:52:11 +0100

    Made comment for sort method in bogosort

--- javaSorts/Bogosort.java
+++ javaSorts/Bogosort.java
@@ -18,6 +18,22 @@ public class Bogosort {
         }
     }
 
+    /**
+     * <h1>Bogosort</h1>
+     * 
+     * <h3>Description</h3>
+     * Bogosort is sorting array by randomly shuffling it until it's sorted. This is also why bogosort isn't an actual
+     * algorithm since it can run forever.
+     * 
+     * <h3>Running Time</h3>
+     * Best Case:\tO(3n + 1) = <b>O(n)</b> \n
+     * Average Case:\t<b>O(n!)</b> \n
+     * Worst Case:\t<b>Infinite</b> \n
+     * 
+     * <h3>Space Complexity & Stability</h3>
+     * Space complexity is: <b>O(1)</b> \n
+     * Bogosort is <b>not stable</b>
+     */
     public static void sort(int[] array) {
         while(!HelperFunctions.isSorted(array)) {
             shuffle(array);
