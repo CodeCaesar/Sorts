3e16ba511df5439cf4f3e05e940c911a16bd3920
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-16 22:58:42 +0100

    Made all functions private except already public heapsort function heapsort.py

--- python/heapsort.py
+++ python/heapsort.py
@@ -1,15 +1,15 @@
 from helper_functions import swap
 
 
-def left(index:int):
+def __left(index:int):
     return (2 * index) + 1
 
-def right(index:int):
+def __right(index:int):
     return (2 * index) + 2
 
-def max_heapify(array:list, index:int, size:int):
-    left_index = left(index)
-    right_index = right(index)
+def __max_heapify(array:list, index:int, size:int):
+    left_index = __left(index)
+    right_index = __right(index)
 
     if left_index < size and array[left_index] > array[index]:
         largest = left_index
@@ -21,22 +21,22 @@ def max_heapify(array:list, index:int, size:int):
     
     if largest != index:
         swap(array, index, largest)
-        max_heapify(array, largest, size)
+        __max_heapify(array, largest, size)
 
-def build_max_heap(array:list, size:int):
+def __build_max_heap(array:list, size:int):
     starting_index = (size // 2) - 1
 
     for index in range(starting_index, -1, -1):
-        max_heapify(array, index, size)
+        __max_heapify(array, index, size)
 
 def __heapsort(array:list, size:int):
-    build_max_heap(array, size)
+    __build_max_heap(array, size)
     heap_size = size
 
     for index in range(size - 1, 0, -1):
         swap(array, 0, index)
         heap_size -= 1
-        max_heapify(array, 0, heap_size)
+        __max_heapify(array, 0, heap_size)
 
 def heapsort(array:list):
     __heapsort(array, len(array))
