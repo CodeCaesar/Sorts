ec3022f704a52e4cb76897ec9e22e79cc0e9ede7
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-17 23:55:32 +0100

    Made comment for CountingSort.java

--- javaSorts/CountingSort.java
+++ javaSorts/CountingSort.java
@@ -13,6 +13,25 @@ public class CountingSort {
         return maxElm;
     }
 
+    /**
+     * <h2>Counting Sort</h2>
+     * <p>
+     * <h3>Description</h3>
+     * Counting sort first gets size of array and max element incremented by 1, then it creates array of size maximum element and empty array, 
+     * then increments index of counting table which stores amount that an element appeared. Then it adds to every index of counting table previous 
+     * amount. Then going backwards it makes sorted array have elements from array at correct place, lastly it copies over all sorted array 
+     * elements to array.
+     * <p>
+     * <h3>Running Time</h3>
+     * Running Time: O(1) + O(n) + O(k) + O(n) + O(n) + O(n) + O(k - 1) + O(k - 1) + O(n) + O(n) + O(n) + O(n) + O(n) = O(9n + 3k - 1) = <b>O(n + k)</b> <p>
+     * Best Case: <b>O(n + k)</b> <p>
+     * Average Case: <b>O(n + k)</b> <p>
+     * Worst Case: <b>O(n + k)</b> <p>
+     * <p>
+     * <h3>Space Complexity & Stability</h3>
+     * Space complexity is: <b>O(k)</b> <p>
+     * Counting Sort is <b>stable</b>
+     */
     public static void sort(int[] array) {
         int arraySize = array.length;
         int maxElm = max_element(array) + 1;
