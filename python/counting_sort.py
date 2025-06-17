fdcd8b32e47fc7cb340182bfd6781cd5309d8b06
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-17 23:50:55 +0100

    Made comment for countingsort.py

--- python/counting_sort.py
+++ python/counting_sort.py
@@ -2,6 +2,25 @@ from helper_functions import max_element
 
 
 def counting_sort(array:list):
+    """
+    <h2>Counting Sort</h2>
+
+    <h3>Description</h3>
+    Counting sort first gets size of array and max element incremented by 1, then it creates array of size maximum element and empty array, 
+    then increments index of counting table which stores amount that an element appeared. Then it adds to every index of counting table previous 
+    amount. Then going backwards it makes sorted array have elements from array at correct place, lastly it copies over all sorted array 
+    elements to array.
+
+    <h3>Running Time</h3>
+    Running Time: O(1) + O(n) + O(k) + O(n) + O(n) + O(n) + O(k - 1) + O(k - 1) + O(n) + O(n) + O(n) + O(n) + O(n) = O(9n + 3k - 1) = <b>O(n + k)</b> \n
+    Best Case: <b>O(n + k)</b> \n
+    Average Case: <b>O(n + k)</b> \n
+    Worst Case: <b>O(n + k)</b> \n
+
+    <h3>Space Complexity & Stability</h3>
+    Space complexity is: <b>O(k)</b> \n
+    Counting Sort is <b>stable</b>
+    """
     array_size = len(array)
     max_elm = max_element(array)
 
