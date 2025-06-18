7813a1219620a56d93d15a9c241e830f3f389881
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-18 19:18:34 +0100

    Made comment for insertionsort.py still need to finish description

--- python/insertionsort.py
+++ python/insertionsort.py
@@ -1,4 +1,21 @@
 def insertion_sort(array:list):
+    """
+    <h2>Insertion Sort</h2>
+
+    <h3>Description</h3>
+    Insertion sort loops
+
+    <h3>Running Time</h3>
+    Running Time: O(n-1) + O(n-1) + O(n-1) + O((n-1)<sup>2</sup>) + O((n-1)<sup>2</sup>) + O((n-1)<sup>2</sup>) + O(n-1) = 
+    O(3(n-1)<sup>2</sup> + 4(n-1)) = O(3n<sup>2</sup> - 2n - 1) = <b>O(n<sup>2</sup>)</b> \n
+    Best Case: <b>O(n)</b> \n
+    Average Case: <b>O(n<sup>2</sup>)</b> \n
+    Worst Case: <b>O(n<sup>2</sup>)</b> \n
+
+    <h3>Space Complexity & Stability</h3>
+    Space complexity is: <b>O(1)</b> \n
+    Insertion Sort is <b>stable</b>
+    """
     for key_index in range(1, len(array)):
         key = array[key_index]
         index = key_index - 1
