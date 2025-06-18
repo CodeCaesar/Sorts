673568b26c7c9489ad7c57d6e820d8bb450a155b
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-18 23:18:53 +0100

    Finished description of insertionsort.py

--- python/insertionsort.py
+++ python/insertionsort.py
@@ -3,7 +3,10 @@ def insertion_sort(array:list):
     <h2>Insertion Sort</h2>
 
     <h3>Description</h3>
-    Insertion sort loops
+    Insertion sort loops through array starting at 2nd index and sets variable key as element of first loop's index, 
+    then sets variable index as key index decremented by 1 and begins while loop. Which moves every element to the right starting 
+    at index all the way to 1st index unless current element is smaller or equal to key, and then it moves key at the end.
+    It repeats it until array is sorted.
 
     <h3>Running Time</h3>
     Running Time: O(n-1) + O(n-1) + O(n-1) + O((n-1)<sup>2</sup>) + O((n-1)<sup>2</sup>) + O((n-1)<sup>2</sup>) + O(n-1) = 
