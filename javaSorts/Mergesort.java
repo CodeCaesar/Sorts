2c25266ac56f177f86e696cd06922dac4ce8735c
CodeCaesar <adrian@czerwinski.info.pl>
2025-06-19 03:18:44 +0100

    Made comment for merge in MergeSort.java

--- javaSorts/Mergesort.java
+++ javaSorts/Mergesort.java
@@ -2,6 +2,15 @@ package javaSorts;
 
 public class Mergesort {
     
+    /**
+     * Merge first gets sizes left_size and right_size then copies two halves into left and right array, next sets last element as max which is 
+     * largest element in array incremented by 1. Then for loop starts from first to last index of array and sorts in ascending order by 
+     * comparing elements of left array to the right array.
+     * <p>
+     * Running Time: O(1) + O(1) + O(n / 2) + O(n / 2) + O(n / 2 - 1) + O(n / 2 - 1) + O(n / 2 - 1) + O(n / 2 - 1) + 
+     * O(1) + O(1) + O(1) + O(1) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) + O(n) = O(7n + 6n/2 - 4 + 6) = O(10n + 2) = 
+     * <b>O(n)</b>
+     */
     private static void merge(int[] array, int first, int pivot, int last) {
         int leftSize = pivot - first + 1;
         int rightSize = last - pivot;
